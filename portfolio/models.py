b323fe1b7eb4213441e8f9d2cfd4d0df5d4dea0e
Abderrazak It <abderrazak.hajji.23ump.ac.ma>
2025-04-23 23:44:20 +0100

    Premier commit : ajout du portfolio

---
+++ portfolio/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    github_link = models.URLField(default='', blank=True) # Non-nullable avec une valeur par défaut
+    demo_link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+    level = models.IntegerField()  # Pourcentage (ex: 80)
+
+    def __str__(self):
+        return self.name
+
+class Experience(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.title} chez {self.company}"
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message de {self.name}"
