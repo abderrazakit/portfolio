1819e1c03e741bd6143ae8c25d4c09ad1a2ea0d1
Abderrazak It <abderrazak.hajji.23ump.ac.ma>
2025-08-23 18:27:06 +0100

    v3

--- projet/wsgi.py
+++ projet/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projet.settings')
 
 application = get_wsgi_application()
+
+#  Hack pour Vercel
+app = application
